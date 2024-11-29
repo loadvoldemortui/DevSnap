0ce21d2ce46710a6a6b549b71d456b9e54dc2097
SukhOP <sukhbircing@gmail.com>
2024-11-30 00:36:50 +0530

    Setup project

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      hello
-    </div>
+    <section className="flex items-center justify-center h-dvh">
+      <div className="container">
+        <h1 className="text-white text-6xl font-bold text-center"><span className="bg-green-500 text-main px-3">Dev</span>Snap</h1>
+      </div>
+    </section>
   );
 }
