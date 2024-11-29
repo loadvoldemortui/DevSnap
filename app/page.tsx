be0a8c970951aeb900a067359ecf93f1f1cc07e5
SukhOP <sukhbircing@gmail.com>
2024-11-30 00:56:46 +0530

    changed project name

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-// import Image from "next/image";
-
 export default function Home() {
   return (
     <section className="flex items-center justify-center h-dvh">
       <div className="container">
-        <h1 className="text-white text-6xl font-bold text-center"><span className="bg-green-500 text-main px-3">Dev</span>Snap</h1>
+        <h1 className="text-white text-6xl font-bold text-center">
+          <p className="text-[200px] leading-none text-green-500">UI</p>
+          Xplore
+        </h1>
       </div>
     </section>
   );
